850fd2b9ddf9139dd05cbf8bc70aebc0a9c43459
venkatasrinivas8688 <pamidisrinivas4@gmail.com>
2025-02-19 18:59:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(express.json());
 
 const port = 5000;
