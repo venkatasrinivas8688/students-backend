c261e09524ccaa8bbb81240adc000055f0346c7b
venkatasrinivas8688 <pamidisrinivas4@gmail.com>
2025-02-20 18:00:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const db = require("./db");
 
 app.listen(port, () => {
